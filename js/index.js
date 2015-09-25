753b2891fdce083752a29480cb73e7e529f6d9cb
Vance Miller <vbmiller@cs.unc.edu>
2015-09-24 21:49:37 -0400

    Reorganized
    Added material design

---
+++ js/index.js
@@ -0,0 +1,188 @@
+
+var socket = io();
+var timeouts = [];
+var chromaMode = false;
+meSpeak.loadConfig("mespeak_config.json");
+meSpeak.loadVoice('en-us.json');
+socket.on('message', function(msg){
+console.log(msg);
+if (msg === "refresh") {
+location.reload(true);
+} else {
+var stream = meSpeak.speak(msg, {"speed":125, 'rawdata': true})
+if (!chromaMode){
+	$('#schedule').fadeOut(100);
+	$('#scroller').slideDown(700);
+}
+$('#blocko').fadeIn(2000);
+$('#message').text(msg);
+$('#blocko').textfill({maxFontPixels:0});
+document.getElementById('audio').play();
+for (var i=0; i<timeouts.length; i++) {
+	clearTimeout(timeouts[i]);
+}
+setTimeout(function(){
+	meSpeak.play(stream);
+}, 1500);
+if (!chromaMode){
+	timeouts.push(setTimeout(function(){
+		$('#blocko').fadeOut(2000);
+		$('#schedule').fadeIn(2000);
+		$('#scroller').fadeOut(2000);
+	}, 300000));
+} else {
+	timeouts.push(setTimeout(function(){
+		$('#blocko').fadeOut(2000);
+	}, 60000));
+}
+}
+});
+$('#room').textfill({maxFontPixels:0});
+
+////////////////////////////////////////////////
+
+var cookieRoom = document.cookie.replace(/(?:(?:^|.*;\s*)room\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+if (cookieRoom) {
+$("#room").html("<span style='position: absolute;bottom:0;'>You're&nbsp;in&nbsp;" + cookieRoom +"</span>").textfill({maxFontPixels:0});
+} else {
+$("#room").html("<span style='position: absolute;bottom:0;'>Welcome&nbsp;to&nbsp;HackNC</span>").textfill({maxFontPixels:0});			
+}
+$(document).keydown(function(event) {
+if (event.which===82) {
+	var roomName = window.prompt("What room is this?", "SN014").replace(" ", "&nbsp;");
+	$("#room").html("<span style='position: absolute;bottom:0;'>You're&nbsp;in&nbsp;" + roomName +"</span>").textfill({maxFontPixels:0});
+	document.cookie = "room=" + roomName
+	$(backgrounds).each(function(){
+		$('<img/>')[0].src = this;
+	});
+};
+if (event.which===67) {
+	chromaMode = true;
+	setBackground();
+	if (chromaMode) {
+		$("#schedule").fadeOut(100);
+		$('#scroller').slideDown(700).css("background-color", "rgba(0,0,0,0)");
+		$('#blocko').css("background-color", "rgba(0,0,0,0)");
+		$('#message').css("text-shadow","1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000").css("color","white");
+	}
+}
+});
+$(document).ready(function(){
+$("#scroller").textfill({maxFontPixels:0})
+});
+
+///////////////////////////////////
+
+<!--
+
+/*By JavaScript Kit
+http://javascriptkit.com
+Credit MUST stay intact for use
+such script kiddie very wow
+*/
+
+function show2(){
+	if (!document.all&&!document.getElementById)
+		return
+	thelement=document.getElementById? document.getElementById("tick2"): document.all.tick2
+	var Digital=new Date()
+	var hours=Digital.getHours()
+	var minutes=Digital.getMinutes()
+	var seconds=Digital.getSeconds()
+	var dn="PM"
+	if (hours<12)
+		dn="AM"
+	if (hours>12)
+		hours=hours-12
+	if (hours==0)
+		hours=12
+	if (minutes<=9)
+		minutes="0"+minutes
+	if (seconds<=9)
+		seconds="0"+seconds
+	var ctime=hours+":"+minutes+":"+seconds+"&nbsp;"+dn
+	thelement.innerHTML="<span style='position: absolute;bottom:0;'>" + ctime + "</span>";
+	$("#tick2").textfill({maxFontPixels:0});
+	setTimeout("show2()",1000)
+}
+window.onload=show2;
+//-->
+
+////////////////////////////
+var schedule = [
+	[new Date("October 25, 2015 11:00:00"), "check in opens"],
+	[new Date("October 25, 2015 11:00:01"), "Career Fair"],
+	[new Date("October 25, 2015 11:30:00"), "lunch with sponsors"],
+	[new Date("October 25, 2015 12:30:00"), "Opening Ceremony (Carroll 111)"],
+	[new Date("October 25, 2015 13:30:00"), "hacking begins!"],
+	[new Date("October 25, 2015 13:30:01"), "Career Fair continues"],
+	[new Date("October 25, 2015 14:00:01"), "HackVR! (FB009)"],
+	[new Date("October 25, 2015 15:00:00"), "Microsoft - Analytics Web APIs (FB009)"],
+	[new Date("October 25, 2015 16:00:00"), "Google - Mesa Tech Talk (FB009)"],
+	[new Date("October 25, 2015 17:00:00"), "Square - Technical Interviewing (FB009)"],
+	[new Date("October 25, 2015 18:00:00"), "Square - Dinner for the ladies (FB009)"],
+	[new Date("October 25, 2015 18:00:01"), "dinner for the everyone (Brooks lobby)"],
+	[new Date("October 25, 2015 19:00:01"), "EMC tech talk (FB009)"],
+	[new Date("October 25, 2015 20:00:01"), "Traditional Smash Tourney (Sitterson Lobby)"],
+	[new Date("October 25, 2015 21:30:01"), "TowerFall Tourney (Sitterson Lobby)"],
+	[new Date("October 25, 2015 22:15:01"), "'Experimental' Smash Tourney (Sitterson Lobby)"],
+	[new Date("October 26, 2015 00:00:01"), "SPECIAL EVENT"],
+	[new Date("October 26, 2015 01:00:00"), "Nap room opens (FB009)"],
+	[new Date("October 26, 2015 07:00:00"), "Breakfast (Brooks lobby)"],
+	[new Date("October 26, 2015 11:30:00"), "Lunch (Brooks lobby)"],
+	[new Date("October 26, 2015 12:00:00"), "Hacking ends"],
+	[new Date("October 26, 2015 12:45:00"), "Expo (Sitterson Lobby)"],
+	[new Date("October 26, 2015 14:30:00"), "Closing ceremony (Carroll 111)"],
+	[new Date("October 26, 2015 16:00:00"), "Time to go... :("],
+	[new Date("October 26, 2015 16:00:00"), "HackNC or something maybe"]	
+];
+
+function getEvent() {
+	var currentTime = new Date();
+	var i = 0;
+	var size = schedule.length;
+	while (schedule[i][0] < currentTime) {
+		i++;
+	}
+	toReturn = [];
+	for (j = i; j < Math.min(i+10, size); j++) {
+		var hoursTil = (schedule[j][0].getTime() - currentTime.getTime()) / 1000 / 60 / 60;
+		if (hoursTil > 1.5) {
+			hoursTil = Math.round(hoursTil);
+			toReturn.push(hoursTil + " hours until " + schedule[j][1]);
+		} else {
+			minutesTil = hoursTil * 60;
+			if (minutesTil > 1.5) {
+				minutesTil = Math.round(minutesTil);
+				toReturn.push(minutesTil + " minutes until " + schedule[j][1]);
+			} else {
+				secondsTil = Math.round(minutesTil * 60);
+				toReturn.push(secondsTil + " second(s) until " + schedule[j][1]);
+			}
+		}
+	}
+	return toReturn.length > 0 ? toReturn : ["See you next time!"]
+}
+
+setInterval(function(){
+	events = getEvent();
+	document.getElementById("scroller").innerHTML = "<span>" + events[0] + "</span>";
+	$("#scroller").textfill({maxFontPixels:0});
+	sString = "";
+	for (i = 0; i < Math.min(8, events.length); i++) {
+		sString += events[i];
+		sString += "<br>";
+	}
+	document.getElementById("schedule").innerHTML = "<span class=message style='margin-left:10%'>" + sString + "</span>";
+	$("#schedule").textfill({maxFontPixels:0});
+}, 1000);
+
+setInterval(setBackground(), 1800000);
+
+function setBackground() {
+	if (!chromaMode){
+		$("body").css("background-image", "url(" + backgrounds[Math.floor(Math.random() * backgrounds.length)] + ")");
+	} else {
+		$("body").css("background-image", "url(./green.png)");
+	}
+}
