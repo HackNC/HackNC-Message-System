134d3e12bb7dfe47672622b646b9140bbede6184
Vance Miller <vbmiller@cs.unc.edu>
2015-09-25 14:15:43 -0400

    update dependencies

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var app = require('express')();
 var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var MailChimpAPI = require('mailchimp').MailChimpAPI;
 var secret = require('./secret.json');
-var apiKey = secret.key;
 var googleKey = secret.googleKey;
 var gcm = require('node-gcm');
 var sender = new gcm.Sender(googleKey);
@@ -13,12 +11,6 @@ var fs = require("fs");
 var regIDs = JSON.parse(fs.readFileSync('regids.txt'));
 
 var messageArchive = JSON.parse(fs.readFileSync('archive.txt'));
-
-try { 
-    var api = new MailChimpAPI(apiKey, { version : '2.0' });
-} catch (error) {
-    console.log(error.message);
-}
 
 //files and stuff
 app.get('/', function(req, res){
@@ -78,21 +70,6 @@ io.on('connection', function(socket){
 		sender.send(notificate, regIDs, 4, function(result) {
 			console.log(result);
 		});
-		try {
-			if (msg.email) {
-				api.call('campaigns', 'create', {type:"plaintext", options:{list_id:secret.list, subject:msg.subject, from_email:"contact@hacknc.us", from_name: "HackNC", to_name:"*|NAME|*"}, content:{text:msg.body}}, function (error, data) {
-					if (error) {
-						console.log(error.message);
-						console.log("error!")
-					} else {
-						console.log(JSON.stringify(data));
-						api.call('campaigns', 'send', {cid: data.id}, function (error, data) {});
-					}
-				});
-			}
-		} catch (e) {
-			socket.emit("message", {'error':e});
-		}
 		msg.pass="";
 		messageArchive.push(msg);
 		fs.writeFile('archive.txt', JSON.stringify(messageArchive),  function(err) {
