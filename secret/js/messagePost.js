370b9c47772947fc9c72b7f7a9d9d358390468d6
jsettlem <jsettlem@cs.unc.edu>
2015-10-01 14:09:36 -0400

    starting draft support

--- secret/js/messagePost.js
+++ secret/js/messagePost.js
@@ -1,4 +1,20 @@
-var socket = io();
+//var socket = io();
+var drafts = [];
+
+var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
 
 var messagePost = function() {
   if ($("#m").val() == '' || $("#s").val() == '') {
@@ -8,8 +24,32 @@ var messagePost = function() {
   socket.emit('message', {
     message: $('#m').val(),
 	  subject: $('#s').val(),
-	  pass:	   $('#pass').val(),
-	  level:	 $('input:radio[name=level]:checked').val()
+	  pass: $('#pass').val(),
+	  level: $('input:radio[name=level]:checked').val()
 	});
   return false;
 };
+
+var addDraft = function() {
+  /*socket.emit('draft', {
+    message: $('#m').val(),
+	  subject: $('#s').val(),
+	  pass: $('#pass').val(),
+	  level: $('input:radio[name=level]:checked').val()
+	});*/
+	nextDraft = {
+		message: $('#m').val(),
+		subject: $('#s').val(),
+		level: $('input:radio[name=level]:checked').val()
+	};
+	drafts.push(nextDraft);
+	updateDrafts();
+}
+
+function updateDrafts() {
+	hizzle = "";
+	for (i = 0; i < drafts.length; i++) {
+		hizzle += "<div class=draft><h2>" + escapeHtml(drafts[i].subject) + "</h2><p>" + escapeHtml(drafts[i].message) + "</p></div><br>";
+	}
+	$("#drafts").html(hizzle);
+}
