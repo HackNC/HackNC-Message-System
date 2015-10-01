c84437204339aea24ea809aac4ba6623544ff079
jsettlem <jsettlem@cs.unc.edu>
2015-10-01 17:00:24 -0400

    Quick mespeak testing, prepare to be deleted

--- secret/js/secret.js
+++ secret/js/secret.js
@@ -1,8 +1,26 @@
 var socket = io();
+meSpeak.loadConfig("mespeak_config.json");
+meSpeak.loadVoice('en-us.json');
 
 socket.on('message', function(msg){
   if (msg.hasOwnProperty("error")){
 	  alert(msg.error);
 	  console.log(msg.error);
   }
+});
+
+demoVoice = function(text) {
+	meSpeak.speak(text, {"speed":125});
+}
+
+$(function(){
+	$("#s").characterCounter({
+		limit: 120
+	})
+	$("#m").characterCounter({
+		limit: 200
+	})
+	$("#v").characterCounter({
+		limit: 200
+	})
 });
