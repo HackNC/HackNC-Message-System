350b88f29b81cef51ea632504f9b9dd40deaf19c
jsettlem <jsettlem@cs.unc.edu>
2015-10-01 14:51:00 -0400

    More draft stufff

--- secret/js/messagePost.js
+++ secret/js/messagePost.js
@@ -1,5 +1,16 @@
 //var socket = io();
 var drafts = [];
+var messageTemplate = '<div class="panel {0}"><div class="panel-heading"><h3 class="panel-title">{1}</h3></div><div class="panel-body">{2}</div></div>';
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match;
+    });
+  };
+}
 
 var entityMap = {
     "&": "&amp;",
@@ -21,12 +32,15 @@ var messagePost = function() {
     alert("Must fill out subject and message. Not sent.");
     return false;
   }
+  
   socket.emit('message', {
     message: $('#m').val(),
 	  subject: $('#s').val(),
 	  pass: $('#pass').val(),
 	  level: $('input:radio[name=level]:checked').val()
 	});
+	$('#m').val("");
+	$('#s').val(""); 
   return false;
 };
 
@@ -37,6 +51,7 @@ var addDraft = function() {
 	  pass: $('#pass').val(),
 	  level: $('input:radio[name=level]:checked').val()
 	});*/
+
 	nextDraft = {
 		message: $('#m').val(),
 		subject: $('#s').val(),
@@ -44,12 +59,24 @@ var addDraft = function() {
 	};
 	drafts.push(nextDraft);
 	updateDrafts();
+	$('#m').val("");
+	$('#s').val(""); 
+}
+
+deleteDraft = function(i) {
+	drafts.splice(i, 1);
+	updateDrafts();
+}
+
+var useDraft = function(i) {
+	$('#m').val(drafts[i].message);
+	$('#s').val(drafts[i].subject); 
 }
 
 function updateDrafts() {
 	hizzle = "";
 	for (i = 0; i < drafts.length; i++) {
-		hizzle += "<div class=draft><h2>" + escapeHtml(drafts[i].subject) + "</h2><p>" + escapeHtml(drafts[i].message) + "</p></div><br>";
+		hizzle = messageTemplate.format("hacknc_panel", escapeHtml(drafts[i].subject), escapeHtml(drafts[i].message)) + "<a href='javascript:useDraft(" +  i  + ")' class='btn btn-primary' id='addDraft" + i + "'>Use this draft</a> <a href='javascript:deleteDraft(" +  i  + ")' class='btn btn-primary' id='deleteDraft" + i + "'>Delete this draft</a>" + hizzle;
 	}
 	$("#drafts").html(hizzle);
 }
